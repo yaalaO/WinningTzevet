9a755bdda3926872688ce1bb72525ad3d83003b0
jessie-vilner <jessie.vilner.46@gmail.com>
2025-01-08 12:17:20 +0200

    test2

---
+++ jessie_testing_stuff.py
@@ -0,0 +1,3 @@
+biggest_id = 5
+all_tables_by_id = {key:0 for key in range(1, int(biggest_id)+1)}
+print(all_tables_by_id)
--- union_radars.py
+++ union_radars.py
@@ -5,10 +5,36 @@ import os
 
 
 
-
-
-
-
 loc_in_dir = "input/with_id/With ID/Target bank data/"
 list_of_csv_dir = [loc_in_dir + i for i in os.listdir(loc_in_dir)]
-print(list_of_csv_dir)
+
+
+
+
+city_dict = {"Ashdod": ASHDOD, "Carmel": CARMEL, "Gosh": GOSH_DAN, "Kiryat": KIRYAT_GAT, "Meron": MERON, "Modiin": MODIIN, "Ofakim": OFAKIM, "Tseelim": TSEELIM, "YABA": YABA}
+
+
+def get_first_name(file_path):
+    base = os.path.basename(file_path)
+    return base.split("_")[0]
+
+
+biggest_id = 0
+all_tables = []
+for csv_dir in list_of_csv_dir:
+    file = load_file(csv_dir, city_dict[get_first_name(csv_dir)])
+    tables = separate_by_ids(file)
+    for table in tables:
+        table.sort_values(by='time', inplace=True)
+        table.reset_index(inplace=True)
+        all_tables.append(table)
+        if table['ID'][0] > biggest_id:
+            biggest_id = table['ID'][0]
+
+all_tables_by_id = {key:0 for key in range(1, int(biggest_id)+1)}
+for table_index in range(len(all_tables)):
+    current_id = all_tables[table_index]['ID'][0]
+    if all_tables_by_id[current_id] == 0:
+        all_tables_by_id[current_id] = table_index
+    else:
+        all_tables_by_id[current_id] = pd.concat([all_tables_by_id[current_id], table_index], axis=0)
