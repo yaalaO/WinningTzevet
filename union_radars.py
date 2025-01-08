29de41b9aed4a983f620d540410527238d8e971a
jessie-vilner <jessie.vilner.46@gmail.com>
2025-01-08 12:26:07 +0200

    test2

--- union_radars.py
+++ union_radars.py
@@ -19,22 +19,28 @@ def get_first_name(file_path):
     return base.split("_")[0]
 
 
-biggest_id = 0
-all_tables = []
+all_tables_by_id = {}
+# biggest_id = 0
+# all_tables = []
 for csv_dir in list_of_csv_dir:
     file = load_file(csv_dir, city_dict[get_first_name(csv_dir)])
     tables = separate_by_ids(file)
     for table in tables:
         table.sort_values(by='time', inplace=True)
         table.reset_index(inplace=True)
-        all_tables.append(table)
-        if table['ID'][0] > biggest_id:
-            biggest_id = table['ID'][0]
+        id = table['ID'][0]
+        if id in all_tables_by_id.keys():
+            all_tables_by_id[id] = pd.concat([all_tables_by_id[id], table], axis=0)
+        else:
+            all_tables_by_id[id] = table
+        # all_tables.append(table)
+        # if table['ID'][0] > biggest_id:
+        #     biggest_id = table['ID'][0]
 
-all_tables_by_id = {key:0 for key in range(1, int(biggest_id)+1)}
-for table_index in range(len(all_tables)):
-    current_id = all_tables[table_index]['ID'][0]
-    if all_tables_by_id[current_id] == 0:
-        all_tables_by_id[current_id] = table_index
-    else:
-        all_tables_by_id[current_id] = pd.concat([all_tables_by_id[current_id], table_index], axis=0)
+# all_tables_by_id = {key:0 for key in range(1, int(biggest_id)+1)}
+# for table_index in range(len(all_tables)):
+#     current_id = all_tables[table_index]['ID'][0]
+#     if all_tables_by_id[current_id] == 0:
+#         all_tables_by_id[current_id] = table_index
+#     else:
+#         all_tables_by_id[current_id] = pd.concat([all_tables_by_id[current_id], table_index], axis=0)
