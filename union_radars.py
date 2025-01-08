06ef079ffb6eefe5471913aaf9d98188501b5a95
jessie-vilner <jessie.vilner.46@gmail.com>
2025-01-08 14:06:28 +0200

    test2

--- union_radars.py
+++ union_radars.py
@@ -3,15 +3,8 @@ import numpy as np
 from main import *
 import os
 
-
-
-loc_in_dir = "input/with_id/With ID/Target bank data/"
-list_of_csv_dir = [loc_in_dir + i for i in os.listdir(loc_in_dir)]
-
-
-
-
-city_dict = {"Ashdod": ASHDOD, "Carmel": CARMEL, "Gosh": GOSH_DAN, "Kiryat": KIRYAT_GAT, "Meron": MERON, "Modiin": MODIIN, "Ofakim": OFAKIM, "Tseelim": TSEELIM, "YABA": YABA}
+city_dict = {"Ashdod": ASHDOD, "Carmel": CARMEL, "Gosh": GOSH_DAN, "Kiryat": KIRYAT_GAT, "Meron": MERON,
+             "Modiin": MODIIN, "Ofakim": OFAKIM, "Tseelim": TSEELIM, "YABA": YABA}
 
 
 def get_first_name(file_path):
@@ -19,20 +12,28 @@ def get_first_name(file_path):
     return base.split("_")[0]
 
 
-all_tables_by_id = {}
 # biggest_id = 0
 # all_tables = []
-for csv_dir in list_of_csv_dir:
-    file = load_file(csv_dir, city_dict[get_first_name(csv_dir)])
-    tables = separate_by_ids(file)
-    for table in tables:
-        table.sort_values(by='time', inplace=True)
-        table.reset_index(inplace=True)
-        id = table['ID'][0]
-        if id in all_tables_by_id.keys():
-            all_tables_by_id[id] = pd.concat([all_tables_by_id[id], table], axis=0)
-        else:
-            all_tables_by_id[id] = table
+
+def combine_all_tables(loc_in_dir):
+    """param should look like this: input/with_id/With ID/Target bank data/"""
+    all_tables_by_id = {}
+    list_of_csv_dir = [loc_in_dir + i for i in os.listdir(loc_in_dir)]
+    for csv_dir in list_of_csv_dir:
+        file = load_file(csv_dir, city_dict[get_first_name(csv_dir)])
+        tables = separate_by_ids(file)
+        for table in tables:
+            table.sort_values(by='time', inplace=True)
+            table.reset_index(inplace=True)
+            id = table['ID'][0]
+            if id in all_tables_by_id.keys():
+                all_tables_by_id[id] = pd.concat([all_tables_by_id[id], table], axis=0)
+            else:
+                all_tables_by_id[id] = table
+
+    return all_tables_by_id
+
+
         # all_tables.append(table)
         # if table['ID'][0] > biggest_id:
         #     biggest_id = table['ID'][0]
@@ -44,3 +45,4 @@ for csv_dir in list_of_csv_dir:
 #         all_tables_by_id[current_id] = table_index
 #     else:
 #         all_tables_by_id[current_id] = pd.concat([all_tables_by_id[current_id], table_index], axis=0)
+
