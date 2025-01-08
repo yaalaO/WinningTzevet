53437d45f1fe27e22022b7cd232bdc02ba203a6f
jessie-vilner <jessie.vilner.46@gmail.com>
2025-01-08 09:54:26 +0200

    test2

--- clustering_kmeans.py
+++ clustering_kmeans.py
@@ -8,21 +8,48 @@ import os.path
 ########################
 
 original_table = load_file(
-    "/input/with_id/With ID/Target point data/Ashdod_with_ID.csv")
+    "input/with_id/With ID/Target bank data/Ashdod_with_ID.csv")
 tables = separate_by_ids(original_table)
 
 
 
-kmeans = KMeans(init="random", n_clusters=4, n_init=10, max_iter=300, random_state=42 )
-kmeans.fit(tables) #Replace your training dataset instead of x_train
-# The lowest SSE value
-print(kmeans.inertia_)
-# Final locations of the centroid
-print(kmeans.cluster_centers_)
-# The number of iterations required to converge
-print(kmeans.n_iter_)
-# first five predicted labels
-print(kmeans.labels_[:5])
+########################
+#        funcs         #
+########################
+
+def extract_max_z_with_time(table):
+    dat = (table[["time", 'z']]).copy()
+    dat.sort_values(by='time', inplace=True)
+    start = dat['time'][0]
+    dat.sort_values(by='z', inplace=True)
+    end = dat['time']
+    max_z = dat['z']
+
+    time_to_max = end - start
+    return max_z, time_to_max
+
+
+print(tables[0]['z'])
+
+
+
+
+
+
+########################
+#     algorithm        #
+########################
+if False:
+    kmeans = KMeans(init="random", n_clusters=4, n_init=10, max_iter=300, random_state=42 )
+    kmeans.fit(tables) #Replace your training dataset instead of x_train
+    # The lowest SSE value
+    print(kmeans.inertia_)
+    # Final locations of the centroid
+    print(kmeans.cluster_centers_)
+    # The number of iterations required to converge
+    print(kmeans.n_iter_)
+    # first five predicted labels
+    print(kmeans.labels_[:5])
 
 
 # init controls the initialization technique. The standard version of the k-means algorithm is implemented by setting init to "random". Setting this to "k-means++" employs an advanced trick to speed up convergence, which you’ll use later.
